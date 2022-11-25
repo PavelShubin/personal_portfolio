bf892de9ce201f966d07471f7fa081ef06bb3058
Pavel Shubin <pavel04shubin@gmail.com>
2022-11-25 23:38:47 +0300

    Third step

--- blog/models.py
+++ blog/models.py
@@ -4,3 +4,6 @@ class Blog(models.Model):
     Title = models.CharField(max_length=200)
     Date = models.DateField()
     Text = models.TextField()
+
+    def __str__(self):
+        return self.title
--- portfolio/views.py
+++ portfolio/views.py
@@ -5,4 +5,3 @@ from .models import Project
 def home(request):
     projects = Project.objects.all()
     return render(request, 'portfolio/home.html', {'projects': projects})
-
