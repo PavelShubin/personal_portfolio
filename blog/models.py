3176d170bda67ba36b3135e6ab8036a6920f27fc
Pavel Shubin <pavel04shubin@gmail.com>
2022-11-25 11:41:54 +0300

    Second step

--- blog/models.py
+++ blog/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 
 class Blog(models.Model):
-    Title = models.CharField(max_length=20)
-    Date = models.DateField(auto_now=True)
-    Text = models.CharField(max_length=250)
+    Title = models.CharField(max_length=200)
+    Date = models.DateField()
+    Text = models.TextField()
